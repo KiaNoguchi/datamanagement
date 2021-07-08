a2c9bd814ab1be9b5cb15814aa50e561862f37e5
NOGUCHI Kia <s1280094@sshsv212.u-aizu.ac.jp>
2021-07-08 23:49:03 +0900

    initial revision

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int i, j, n;
+    char a[256];
+
+    srand((unsigned)time(NULL));
+
+    printf("あなたの名前は何ですか？\n");
+    scanf("%s", &a);
+    printf("こんにちは、%s", a);
+    printf("サイコロを振ってみると...\n");
+
+    i = rand() % 6 + 1;
+    printf("Die1：%d\n", i);
+    j = rand() % 6 + 1;
+    printf("Die2：%d\n", j);
+
+    printf("total value：%d\n", i + j);
+    printf("%s won!\n",a);
+
+    return 0;
+}
